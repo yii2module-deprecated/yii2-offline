c106fc8a3a5f5733a2eeff48fef389a48ce6fa2a
Vitaliy <theyamshikov@yandex.ru>
2018-01-08 23:13:37 +0600

    move filter

---
+++ src/domain/filters/IsOffline.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace yii2module\offline\domain\filters;
+
+use yii\base\BaseObject;
+use yii2lab\misc\interfaces\FilterInterface;
+use yii2mod\helpers\ArrayHelper;
+
+class IsOffline extends BaseObject implements FilterInterface {
+
+	public function run($config) {
+		$exclude = ArrayHelper::getValue($config, 'params.offline.exclude', []);
+		if(in_array(APP, $exclude)) {
+			unset($config['catchAll']);
+		}
+		return $config;
+	}
+	
+}
