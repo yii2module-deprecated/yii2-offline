0439e7766f7620e5096ec3bcf9d937858c5768da
Vitaliy <theyamshikov@yandex.ru>
2018-02-19 20:11:32 +0600

    make exclude param

--- src/domain/filters/IsOffline.php
+++ src/domain/filters/IsOffline.php
@@ -4,13 +4,13 @@ namespace yii2module\offline\domain\filters;
 
 use yii\base\BaseObject;
 use yii2lab\designPattern\filter\interfaces\FilterInterface;
-use yii2mod\helpers\ArrayHelper;
 
 class IsOffline extends BaseObject implements FilterInterface {
 
+    public $exclude = [];
+
 	public function run($config) {
-		$exclude = ArrayHelper::getValue($config, 'params.offline.exclude', []);
-		if(in_array(APP, $exclude)) {
+		if(in_array(APP, $this->exclude)) {
 			unset($config['catchAll']);
 		}
 		return $config;
