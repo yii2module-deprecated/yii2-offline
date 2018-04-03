6548d9670db19e13c6ee7f7dd9a91f1be07355a3
Vitaliy <theyamshikov@yandex.ru>
2018-04-03 10:08:12 +0600

    use domain locator

--- src/admin/controllers/DefaultController.php
+++ src/admin/controllers/DefaultController.php
@@ -23,9 +23,9 @@ class DefaultController extends Controller
 				ConfigHelper::setState($mode);
 				$modeStr = $mode ? 'offline' : 'online';
 				if($mode == $currentMode) {
-					Yii::$app->navigation->alert->create(['offline/main', 'mode_' . $modeStr . '_already_selected'], Alert::TYPE_WARNING);
+					Yii::$domain->navigation->alert->create(['offline/main', 'mode_' . $modeStr . '_already_selected'], Alert::TYPE_WARNING);
 				} else {
-					Yii::$app->navigation->alert->create(['offline/main', 'success_' . $modeStr], Alert::TYPE_SUCCESS);
+					Yii::$domain->navigation->alert->create(['offline/main', 'success_' . $modeStr], Alert::TYPE_SUCCESS);
 				}
 				return $this->refresh();
 			}
