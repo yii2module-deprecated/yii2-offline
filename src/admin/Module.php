1a608590d3bc5e2930bba0e3cff0fbb0f1518b7a
Vitaliy <theyamshikov@yandex.ru>
2018-08-11 22:21:11 +0600

    upgrade rbac

--- src/admin/Module.php
+++ src/admin/Module.php
@@ -3,6 +3,8 @@
 namespace yii2module\offline\admin;
 
 use yii\base\Module as YiiModule;
+use yii2lab\helpers\Behavior;
+use yii2module\offline\domain\enums\OfflinePermissionEnum;
 
 /**
  * offline module definition class
@@ -11,5 +13,12 @@ class Module extends YiiModule
 {
 
 	public static $langDir = '@yii2module/offline/admin/messages';
+
+    public function behaviors()
+    {
+        return [
+            'access' => Behavior::access(OfflinePermissionEnum::MANAGE),
+        ];
+    }
 
 }
